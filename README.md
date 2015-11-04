c4a31c84f11eeec1735efed16fd29b75ef36dfd1
Kun Xia <me@kunxia.com>
2015-11-04 21:17:30 +0800

    Integrated Pushbullet

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # scheduler
 An integration of Quartz Scheduler with MySQL, used for Airbnb reservation notification powered by Twillo.
-
-About this version:
-
-Integrated with Pushbullet.
---
+++ src/main/java/com/kunxia/scheduler/job/CheckPushesJob.java
@@ -0,0 +1,40 @@
+package com.kunxia.scheduler.job;
+
+import java.util.List;
+
+import net.iharder.jpushbullet2.Push;
+
+import org.apache.log4j.Logger;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import com.kunxia.scheduler.App;
+import com.kunxia.scheduler.pushbullet.PushBulletServiceImpl;
+
+public class CheckPushesJob implements Job{
+	private static final Logger logger = Logger.getLogger(CheckPushesJob.class);
+
+	@Override
+	public void execute(JobExecutionContext context)
+			throws JobExecutionException {
+		PushBulletServiceImpl pushBulletService = (PushBulletServiceImpl) App.appContext.getBean("pushBulletService");
+		List<Push> pushes = pushBulletService.retrievePushes();
+		if(pushes!=null){
+			logger.info("Received "+pushes.size()+" pushes");
+			for(Push push:pushes){
+				String type = push.getType();
+				if(type!=null){
+					if(type.equalsIgnoreCase("link")){
+						logger.info("Push Type:"+push.getType()+" Push content:"+push.getUrl());
+					}else if(type.equalsIgnoreCase("note")){
+						logger.info("Push Type:"+push.getType()+" Push content:"+push.getBody());
+					}else{
+						logger.info("Ignored unsupported push type:"+push.getType());
+					}
+				}
+				
+			}
+		}
+	}
+}
